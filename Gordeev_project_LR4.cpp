178a69aacab6be84b913494ae815fff49e6183f0
14kheaven <feelsgood00@yandex.ru>
2025-04-04 09:17:18 +0300

    first commit of lr4

---
+++ Gordeev_project_LR4.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+#include <functional>
+#include <string>
+using namespace std;
+
+int side1, side2;
+
+bool UserInput(string input) {
+    if (input.empty()) return false;
+    try {
+        int number = stoi(input);
+        if (number < 0) return false;
+    }
+catch (...)
+{ return false; }
+return true;
+}
+
+void enterNumber(int& varLink, string label) {
+    string str_input;
+    cout << label << " = ";
+    getline(cin, str_input);
+    while (!UserInput(str_input)) {
+        cout << label << " = ";
+        getline(cin, str_input);
+    }
+    varLink = stoi(str_input);
+}
+
+void enterSide1() {
+
+}
+
+void enterSide2() {
+
+}
+
+void calcS() {
+
+}
+
+void calcP() {
+
+}
+
+struct MenuItem {
+    string title;
+    function<void()> action;
+};
+
+int main() {
+    map<int, MenuItem> menu = {
+        {1, {"Ввод 1-й стороны прямоугольника", enterSide1}},
+        {2, {"Ввод 2-й стороны прямоугольника", enterSide2}},
+        {3, {"Площадь прямоугольника", calcS}},
+        {4, {"Периметр прямоугольника", calcP}},
+    };
+int choice = 0;
+
+while (true) {
+    cout << "Меню:" << endl;
+    for (const auto& item : menu) {
+        cout << "Task " << item.first << ". " << item.second.title << endl;
+    }
+    cout << "0. Выход" << endl;
+    enterNumber(choice, "Input number of menu: ");
+    if (choice == 0) {
+        cout << "© 2025 Ilya Gordeev" << endl;
+        break;
+    }
+    cout << endl;
+    if (menu.find(choice) != menu.end()) {
+        menu[choice].action();
+    } else {
+        cout << "Некорректный ввод.";
+    }
+    cout << endl;
+}
+    return 0;
+}
